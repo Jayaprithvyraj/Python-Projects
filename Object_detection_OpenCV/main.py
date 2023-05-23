c0a660eb5943d0762451aae8bb647d85d379d3eb
Jayaprithvyraj <46399449+Jayaprithvyraj@users.noreply.github.com>
2023-05-23 14:01:19 +0530

    updated_main.py
    Added sound modules to narrate object found

--- Object_detection_OpenCV/main.py
+++ Object_detection_OpenCV/main.py
@@ -4,7 +4,17 @@ from  cvlib.object_detection import draw_bbox
 from gtts import gTTS
 from playsound import playsound
 
+
+def speech(text):
+    print(text)
+    language = "en"
+    output = gTTS(text = text, lang=language, slow=False)
+    
+    output.save("./sounds/output.mp3")
+    playsound("./sounds/output.mp3")
+
 video = cv2.VideoCapture(1)
+labels = []
 
 while True:
     ret, frame = video.read()
@@ -13,5 +23,23 @@ while True:
 
     cv2.imshow("Object Detection", output_image)
 
+    for item in label:
+        if item in labels:
+            pass
+        else:
+            labels.append(item)
+
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+
+i = 0
+new_sentence = []
+for label in labels:
+    if i == 0:
+        new_sentence.append(f"I found a {label}")
+    else:
+        new_sentence.append(f", a {label}")
+    
+    i += 1
+
+speech("".join(new_sentence))
